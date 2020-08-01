19035bc3fc4e0e60eecc48f99c58ed64d8a610da
PacNiKK <dominik.alberts@outlook.com>
2020-08-01 13:15:47 +0200

    fixed array loading

--- filehandler.py
+++ filehandler.py
@@ -5,6 +5,13 @@ import numpy as np
 
 #imports a yaml list in 'path' and returns it as an array
 def load_scene(path):
+	array ('B'[])
+	int i=0
+	data.items()[0][1]
 	with open(path) as f:
 		data=yaml.load(f, Loader=yaml.FullLoader)
-	return np.asarray(data.items()[0][1],dtype=np.int)
+	list=data.items()[0][1]
+	for channel in list:
+		array[i]=channel
+		i+=1
+	return array
