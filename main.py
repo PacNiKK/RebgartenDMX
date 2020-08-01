a0279094780aee8254a01cd943d9021153abaebf
PacNiKK <dominik.alberts@outlook.com>
2020-08-01 11:35:56 +0200

    load all scenes from yaml files

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 import ola
 from filehandler import load_scene
-path='scenes/sceneA.yaml'
-print(load_scene(path))
+path='scenes/'
+sceneA=load_scene(path+'sceneA.yaml')
+sceneB=load_scene(path+'sceneB.yaml')
+sceneC=load_scene(path+'sceneC.yaml')
+sceneD=load_scene(path+'sceneD.yaml')
+sceneE=load_scene(path+'sceneE.yaml')
+
+print(sceneA)
+print(sceneB)
+print(sceneC)
+print(sceneD)
+print(sceneE)
