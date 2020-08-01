eac6cebd4d2b459822f5c8574e64f3e880bbcff8
PacNiKK <dominik.alberts@outlook.com>
2020-08-01 12:25:17 +0200

    changed return to array instead of list

--- filehandler.py
+++ filehandler.py
@@ -1,8 +1,10 @@
 #filehandler.py
 import yaml
+import array
+from numpy import array
 
-#imports a yaml list in 'path' and returns it as a list
+#imports a yaml list in 'path' and returns it as an array
 def load_scene(path):
 	with open(path) as f:
 		data=yaml.load(f, Loader=yaml.FullLoader)
-	return data.items()[0][1]
+	return array(data.items()[0][1])
