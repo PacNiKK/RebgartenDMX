9ba475eb8683821dde1e31fc2bd25b7c7fc03aa1
PacNiKK <dominik.alberts@outlook.com>
2020-08-01 11:25:10 +0200

    Update filehandler.py

--- filehandler.py
+++ filehandler.py
@@ -1,8 +1,7 @@
 import yaml
-def load_scene(path)
-with open(path) as f:
-
-	data=yaml.load(f, Loader=yaml.FullLoader)
-	return data.items()[0][1]
+def load_scene(path):
+	with open(path) as f:
+		data=yaml.load(f, Loader=yaml.FullLoader)
+		return data.items()[0][1]
 
 
