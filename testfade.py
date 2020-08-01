87af55fa2793b6e0c1fa9e2ad4dd77504079609e
PacNiKK <dominik.alberts@outlook.com>
2020-08-01 14:54:33 +0200

    Update testfade.py

--- testfade.py
+++ testfade.py
@@ -1,53 +1,51 @@
-from array import array
+import time
+from filehandler import load_scene
 from ola.ClientWrapper import ClientWrapper
-from ola.DMXConstants import DMX_MIN_SLOT_VALUE, DMX_MAX_SLOT_VALUE, \
-    DMX_UNIVERSE_SIZE
 
-__author__ = 'Sean Sill'
+#path to scene files
+path='scenes/'
 
-"""
-This script fades DMX_DATA_SIZE channels from 0 to 255. It serves as an example
-of how to use AddEvent to schedule dmx data updates from python
-To view data, use the web interface or patch an output device to the same
-universe
-"""
+universe=1
+frame=50
 
+#load yaml files into lists
+sceneA=load_scene(path+'sceneA.yaml')
+sceneB=load_scene(path+'sceneB.yaml')
+sceneC=load_scene(path+'sceneC.yaml')
+sceneD=load_scene(path+'sceneD.yaml')
+sceneE=load_scene(path+'sceneE.yaml')
 
-UPDATE_INTERVAL = 25  # In ms, this comes about to ~40 frames a second
-SHUTDOWN_INTERVAL = 10000  # in ms, This is 10 seconds
-DMX_DATA_SIZE = 24
-UNIVERSE = 1
+stage=sceneA
+#scene_change(stage)
 
+print(sceneA)
+print(sceneB)
+print(sceneC)
+print(sceneD)
+print(sceneE)
 
-class SimpleFadeController(object):
-  def __init__(self, universe, update_interval, client_wrapper,
-               dmx_data_size=DMX_UNIVERSE_SIZE):
-    dmx_data_size = min(dmx_data_size, DMX_UNIVERSE_SIZE)
-    self._universe = universe
-    self._update_interval = update_interval
-    self._data = array('B', [DMX_MIN_SLOT_VALUE] * dmx_data_size)
-    self._wrapper = client_wrapper
-    self._client = client_wrapper.Client()
-    self._wrapper.AddEvent(self._update_interval, self.UpdateDmx)
+wrapper = None
+loop_count = 0
+TICK_INTERVAL = 100  # in ms
 
-  def UpdateDmx(self):
-    """
-    This function gets called periodically based on UPDATE_INTERVAL
-    """
-    for i in range(len(self._data)):
-      self._data[i] = (self._data[i] + 1) % DMX_MAX_SLOT_VALUE
-    # Send the DMX data
-    self._client.SendDmx(self._universe, self._data)
-    # For more information on Add Event, reference the OlaClient
-    # Add our event again so it becomes periodic
-    self._wrapper.AddEvent(self._update_interval, self.UpdateDmx)
+def DmxSent(state):
+  if not state.Succeeded():
+    wrapper.Stop()
 
+def SendDMXFrame():
+  # schdule a function call in 100ms
+  # we do this first in case the frame computation takes a long time.
+  wrapper.AddEvent(TICK_INTERVAL, SendDMXFrame)
 
-if __name__ == '__main__':
-  wrapper = ClientWrapper()
-  controller = SimpleFadeController(UNIVERSE, UPDATE_INTERVAL, wrapper,
-                                    DMX_DATA_SIZE)
-  # Call it initially
-  wrapper.AddEvent(SHUTDOWN_INTERVAL, wrapper.Stop)
-  # Start the wrapper
-  wrapper.Run()
+  # compute frame here
+  data = array.array('B')
+  global loop_count
+  data.append(loop_count % 255)
+  loop_count += 1
+
+  # send
+  wrapper.Client().SendDmx(1, data, DmxSent)
+
+wrapper = ClientWrapper()
+wrapper.AddEvent(TICK_INTERVAL, SendDMXFrame)
+wrapper.Run()
