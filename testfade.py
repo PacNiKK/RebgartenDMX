edfcdfe5febd377f41fbaf2d834bfc079dd2651d
PacNiKK <dominik.alberts@outlook.com>
2020-08-01 16:05:09 +0200

    Update testfade.py

--- testfade.py
+++ testfade.py
@@ -1,9 +1,5 @@
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-def button_callback(channel):
-	print("Button was pushed!")
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 3 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(3,GPIO.RISING,callback=button_callback) # Setup event on pin 3 rising edge
-message = input("Press enter to quit\n\n") # Run until someone presses enter
-GPIO.cleanup() # Clean up
+from gpiozero import LED, Button
+from signal import pause
+button=Button(3)
+button.when_pressed=print("Button was pressed")
+pause()
