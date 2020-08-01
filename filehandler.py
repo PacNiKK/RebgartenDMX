436f64f6aee48ce5b3e863df07aaba0749a7137c
PacNiKK <dominik.alberts@outlook.com>
2020-08-01 12:39:52 +0200

    Update filehandler.py

--- filehandler.py
+++ filehandler.py
@@ -1,17 +1,10 @@
 #filehandler.py
 import yaml
 import array
+import numpy as np
 
 #imports a yaml list in 'path' and returns it as an array
 def load_scene(path):
-	
 	with open(path) as f:
 		data=yaml.load(f, Loader=yaml.FullLoader)
-		
-		return_list=';'.join(data.items()[0][1])
-		return_array = []
-		for item in return_list.split(';'):
-			return_array.append(item)
-	return return_array
-	
-	
+	return np.asarray(data.items()[0][1])
