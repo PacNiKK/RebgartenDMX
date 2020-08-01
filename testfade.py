20e92e66389a10a6079af1378cccc26253892d13
PacNiKK <dominik.alberts@outlook.com>
2020-08-01 15:47:41 +0200

    Update testfade.py

--- testfade.py
+++ testfade.py
@@ -3,7 +3,7 @@ def button_callback(channel):
 	print("Button was pushed!")
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 4 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(4,GPIO.RISING,callback=button_callback) # Setup event on pin 4 rising edge
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 3 to be an input pin and set initial value to be pulled low (off)
+GPIO.add_event_detect(3,GPIO.RISING,callback=button_callback) # Setup event on pin 3 rising edge
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 GPIO.cleanup() # Clean up
