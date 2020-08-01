ac19320d1ceca2726a2c69d522f0f3c634cca64a
PacNiKK <dominik.alberts@outlook.com>
2020-08-01 12:04:37 +0200

    Added comments and started scene_fade function

--- filehandler.py
+++ filehandler.py
@@ -1,5 +1,7 @@
 #filehandler.py
 import yaml
+
+#imports a yaml list in 'path' and returns it as a list
 def load_scene(path):
 	with open(path) as f:
 		data=yaml.load(f, Loader=yaml.FullLoader)
--- main.py
+++ main.py
@@ -1,14 +1,31 @@
-import ola
+import array
 from filehandler import load_scene
+from ola.ClientWrapper import ClientWrapper
+
+#path to scene files
 path='scenes/'
+
+universe=1
+
+#load yaml files into lists
 sceneA=load_scene(path+'sceneA.yaml')
 sceneB=load_scene(path+'sceneB.yaml')
 sceneC=load_scene(path+'sceneC.yaml')
 sceneD=load_scene(path+'sceneD.yaml')
 sceneE=load_scene(path+'sceneE.yaml')
 
-print(sceneA)
-print(sceneB)
-print(sceneC)
-print(sceneD)
-print(sceneE)
+print('load scene A: '+sceneA)
+print('load scene B: '+sceneB)
+print('load scene C: '+sceneC)
+print('load scene D: '+sceneD)
+print('load scene E: '+sceneE)
+
+
+def DmxSent(state):
+	wrapper.Stop()
+	
+#change to 'scene'
+def scene_change(scene, time):
+pass	
+	
+
